2ea70f52bee3f711a51db46d31ba664567aebedc
Matia <matia.casu@gmail.com>
2021-03-05 19:45:59 +0100

    Logica Ingredienti aggiunti a prezzo

--- js/script.js
+++ js/script.js
@@ -3,17 +3,30 @@
 - Attribuire valore ad ingredienti e sommarlo al prezzo base se "attivati"
 - Creare una lista di codici in cui ricercare quello del coupon utente
 - Stampare prezzo base(50)+ingredienti+coupon
- */
+*/
 
 
 // Chiedere il nome dell'hamburger all'utente prima di poter eseguire calcolo
 var nBurger = document.getElementById('n_burger');
 var compute = document.getElementById('compute');
+var ingredienti = document.getElementsByClassName('ingredients');
+var burgerPrice = document.getElementsByClassName('price');
 
 compute.addEventListener("click", function(){
   if(nBurger.value === ""){
-  compute = alert("nome vuoto");
+    compute = alert("Dai un nome al tuo Hamburger!");
   }
-  console.log(nBurger.value);
+  else{
+    var plainPrice = 50;
+    for(var i = 0; i <ingredienti.length; i++){
+      ingredienti[i]=i;
+
+      if(ingredienti[i].checked){
+        plainPrice += parseInt(ingredienti[i].value);
+      }
+    }
+    price.innerHTML = plainPrice;
+  }
+
 
 });
